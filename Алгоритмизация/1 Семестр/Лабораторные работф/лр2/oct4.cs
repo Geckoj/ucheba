081de83385e3803658b1de422c0bdf43f8b18772
Geckoj <geckoyaa@gmail.com>
2024-10-04 17:40:29 +0600

    Update oct4.cs

--- Алгоритмизация/1 Семестр/Лабораторные работф/лр2/oct4.cs
+++ Алгоритмизация/1 Семестр/Лабораторные работф/лр2/oct4.cs
@@ -1,120 +1,89 @@
 using System;
 
-public class HelloWorld
+class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        int a = Convert.ToInt32(Console.ReadLine());
-        int b = Convert.ToInt32(Console.ReadLine()); 
-        int mem1 = 0;
-        int mem2 = 0;
-        int mem3 = 0;
-        int chet = 1;
-        int prev= 0;
-        int counter1 = 0;
-        int min1 = 1000000;
-        int counter2 = 0;
-        int counter3 = 0;
-        int max2 = 0;
-        int max3 = 0;
 
-        counter1 = isOne(a, counter1);
-        counter2 = isEvenAndEqual(a, b, prev, counter2);
-        counter3 = isEvenSum(a, counter3);
- 
-        while (n >= 1)
+        int previousElement = 0;
+        int currentElement;
+
+        int maxEqualEvenCount = 0;
+        int equalEvenCount = 0;
+
+        int oneCount = 0;
+        int minOneCount = int.MaxValue;
+        int memOne = 0;
+
+        int maxEvenSumm = 0;
+        int evenSumm = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            n--;
-            
-            prev = a;
-            a=b;
-            b = Convert.ToInt32(Console.ReadLine());
-            
-            mem1 = counter1;
-            counter1 = isOne(a, counter1);
-            
-            if (counter1 == 0 && mem1!= 0)
+            currentElement = Convert.ToInt32(Console.ReadLine());
+
+            if (currentElement % 2 == 0)
             {
-              min1 = Math.Min(min1, mem1);
+                if (currentElement == previousElement)
+                {
+                    equalEvenCount++;
+                }
+                else
+                {
+                    equalEvenCount = 1;
+                }
+                maxEqualEvenCount = Math.Max(maxEqualEvenCount, equalEvenCount);
             }
-            
-            
-            mem2 = counter2;
-            counter2 = isEvenAndEqual(a, b, prev, counter2);
-            
-            if ((counter2 == 0 && mem2!= 0) || (n == 0))
+            else
             {
-              max2 = Math.Max(max2, mem2);
+                equalEvenCount = 0;
             }
+
+
+            //================================//
+            memOne = oneCount;
             
-            mem3 = counter3;
-            counter3 = isEvenSum(a, counter3);
-            
-            if ((counter3 == 0 && mem3!= 0) || (n == 0))
+            if (currentElement == 1)
             {
-              max3 = Math.Max(max3, mem3);
+                oneCount++;
             }
-            
-            // Console.WriteLine($"{n} {a} {b}     {counter3}, {mem3}, {max3}");
-            
-            
+            else
+            {
+                oneCount = 0;
+            }
+
+            if (oneCount == 0 && memOne != 0)
+            {
+                minOneCount = Math.Min(minOneCount, memOne);
+            }
+
+            previousElement = currentElement;
+            Console.WriteLine($"cur: {oneCount},  min: {minOneCount},  mem: {memOne}");
+
+            //===============================//
+
+            if (currentElement % 2 == 0)
+            {
+                evenSumm += currentElement;
+                maxEvenSumm = Math.Max(maxEvenSumm, evenSumm);
+
+            }
+            else
+            {
+                evenSumm = 0;
+            }
+
         }
-        if (min1 == 1000000)
+        if (minOneCount == int.MaxValue)
         {
-          min1 = 0;
+            minOneCount = Math.Min(minOneCount, oneCount);
         }
 
-        Console.WriteLine($"минимальный размер подпоследовательности единиц {min1}");
-        Console.WriteLine($"максимальный размер подпоследовательности четных одинаковых элементов {max2}");
-        Console.WriteLine($"максимальная сумма подпоследовательности из четных элементов {max3}");
-        
-        }
-    static int isEvenAndEqual(int a, int b, int prev, int chet)
-    {
-        chet = chet;
-        if (((a % 2 == 0) && (b % 2 == 0) && (a == b)) || ((a % 2 == 0) && (prev % 2 == 0) && (a == prev)))
-        {
-          chet += 1;
-        }
-        else
-        {
-          chet = 0;
-        }
-        
-        return chet;
-    }
-    
-    static int isEvenSum(int a, int chet)
-    {
-      chet = chet;
-      
-      if (a % 2 == 0)
-      {
-        chet += a;
-      }
-      else
-      {
-        chet = 0;
-      }
-      
-      return chet;
-    }
-    
-    static int isOne(int a, int counter)
-    {
-      counter = counter;
-      
-      if (a == 1)
-      {
-        counter += 1;
-      }
-      else
-      {
-        counter = 0;
-      }
-      
-      return counter;
-    }
+        maxEvenSumm = Math.Max(evenSumm, maxEvenSumm);
 
+        Console.WriteLine($"минимальная длина последовательности из единиц: {minOneCount}");
+        Console.WriteLine($"максимальная длина четный равных элементов: {maxEqualEvenCount}");
+        Console.WriteLine($"максимальная сумма идущих подряд четных элеметов: {maxEvenSumm}");
     }
+}
